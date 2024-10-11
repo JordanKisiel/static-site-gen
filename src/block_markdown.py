dae95826d8d516e7a2fa8c4eae79ed6af98e3527
JordanKisiel <jordan.kisiel@gmail.com>
2024-10-11 12:17:26 -0400

    added classification of markdown blocks

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,5 +1,12 @@
 import re
 
+block_type_paragraph= "paragraph"
+block_type_heading = "heading"
+block_type_quote = "quote"
+block_type_code = "code"
+block_type_unordered_list = "unordered list"
+block_type_ordered_list = "ordered list"
+
 def markdown_to_block(markdown):
     # create list of blocks to return
     resulting_blocks = []
@@ -17,3 +24,44 @@ def markdown_to_block(markdown):
             resulting_blocks.append(resulting_block)
 
     return resulting_blocks
+
+def block_to_block_type(md_block):
+    heading_match = re.fullmatch(r"#{1,6}[ ].*", md_block)
+    if heading_match != None:
+        return block_type_heading
+    
+    code_match = re.fullmatch(r"`{3}(.|\n)*`{3}", md_block)
+    if code_match != None:
+        return block_type_code
+    
+    lines = md_block.split("\n")
+    quote_matches = []
+    for line in lines:
+        line_match= re.fullmatch(r">.*", line)
+        if line_match != None:
+            quote_matches.append(line_match)
+    if len(quote_matches) == len(lines):
+        return block_type_quote
+    
+    unordered_matches = []
+    for line in lines:
+        line_match = re.fullmatch(r"[*-][ ].*", line)
+        if line_match != None:
+            unordered_matches.append(line_match)
+    if len(unordered_matches) == len(lines):
+        return block_type_unordered_list
+    
+    ordered_matches = []
+    for line in lines:
+        line_match = re.fullmatch(r"\d[.][ ].*", line)
+        if line_match != None:
+            ordered_matches.append(line_match)
+    if len(ordered_matches) == len(lines):
+        return block_type_ordered_list
+    
+    return block_type_paragraph
+
+    
+
+
+
--- src/tests/test_block_markdown.py
+++ src/tests/test_block_markdown.py
@@ -37,5 +37,155 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
             markdown_to_block(text2)
         )
 
+    def test_block_to_block_type(self):
+        heading_block1 = "# This is a heading"
+        heading_block2 = "###### This is a heading too"
+        heading_block3 = "####### This is NOT a heading"
+        heading_block4 = "#*# This is also NOT a heading"
+
+        self.assertEqual(
+            block_to_block_type(heading_block1),
+            block_type_heading
+        )
+
+        self.assertEqual(
+            block_to_block_type(heading_block2),
+            block_type_heading
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(heading_block3),
+            block_type_heading
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(heading_block4),
+            block_type_heading
+        )
+
+        code_block1 = "```This is a single line code block```"
+        code_block2 = "```This is a multiline\nCode block\nYep```"
+        code_block3 = "```This is not a code block"
+        code_block4 = "This is also not a code block```"
+        code_block5 = "```This is also not a ```code block"
+
+        self.assertEqual(
+            block_to_block_type(code_block1),
+            block_type_code
+        )
+
+        self.assertEqual(
+            block_to_block_type(code_block2),
+            block_type_code
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(code_block3),
+            block_type_code
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(code_block4),
+            block_type_code
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(code_block5),
+            block_type_code
+        )
+
+        quote_block1 = ">This is a quote block\n> Yes indeed\n>it sure is"
+        quote_block2 = ">This is NOT a quote block\nNope indeed\n>it sure isn't"
+        quote_block3 = "This is NOT a quote block\n>Nope indeed\n>it sure isn't"
+        quote_block4 = ">This is NOT a quote block\n>Nope indeed\nit sure isn't"
+
+        self.assertEqual(
+            block_to_block_type(quote_block1),
+            block_type_quote
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(quote_block2),
+            block_type_quote
+        ) 
+        
+        self.assertNotEqual(
+            block_to_block_type(quote_block3),
+            block_type_quote
+        ) 
+
+        self.assertNotEqual(
+            block_to_block_type(quote_block4),
+            block_type_quote
+        )
+
+        unordered_block1 = "* This is an ul block\n- Yep it sure is\n* Yep"
+        unordered_block2 = " This is NOT an ul block\n- Yep it sure is\n* Yep"
+        unordered_block3 = "*This is NOT an ul block\n- Yep it sure is\n* Yep"
+        unordered_block4 = "* This is NOT an ul block\n+ Yep it sure is\n* Yep"
+
+        self.assertEqual(
+            block_to_block_type(unordered_block1),
+            block_type_unordered_list
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(unordered_block2),
+            block_type_unordered_list
+        ) 
+        
+        self.assertNotEqual(
+            block_to_block_type(unordered_block3),
+            block_type_unordered_list
+        ) 
+
+        self.assertNotEqual(
+            block_to_block_type(unordered_block4),
+            block_type_unordered_list
+        )
+
+        ordered_block1 = "1. this is an\n2. list\n3. Yep"
+        ordered_block2 = "1.this is NOT an\n2. list\n3. Yep"
+        ordered_block3 = "1. this is NOT an\n2 list\n3. Yep"
+        ordered_block4 = "1. this is NOT an\n2. list\na. Yep"
+
+        self.assertEqual(
+            block_to_block_type(ordered_block1),
+            block_type_ordered_list
+        )
+
+        self.assertNotEqual(
+            block_to_block_type(ordered_block2),
+            block_type_ordered_list
+        ) 
+        
+        self.assertNotEqual(
+            block_to_block_type(ordered_block3),
+            block_type_ordered_list
+        ) 
+
+        self.assertNotEqual(
+            block_to_block_type(ordered_block4),
+            block_type_ordered_list
+        )
+
+        para_block = "This is a just a regular\nparagraph\nYep"
+
+        self.assertEqual(
+            block_to_block_type(para_block),
+            block_type_paragraph
+        )
+
+
+         
+        
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
     unittest.main()
